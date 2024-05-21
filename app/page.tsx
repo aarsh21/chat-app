a7f9ed5e9c810008b381b0b6181fe92efc0a313c
aarsh21 <aarshpersonal21@gmail.com>
2024-05-21 11:41:05 +0530

    initial commit
    Signed-off-by: aarsh21 <aarshpersonal21@gmail.com>

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-5xl text-red-400 text-center">hello</h1>
+      <div>
+        <Button>ewoo</Button>
+      </div>
+    </div>
+  );
+}
