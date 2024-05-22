42b9637389729c66032f06e3bbcd9557ad81e07f
aarsh21 <aarshpersonal21@gmail.com>
2024-05-22 12:21:56 +0530

    add modal and form fields for server creation
    Signed-off-by: aarsh21 <aarshpersonal21@gmail.com>

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,3 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { initialProfile } from "@/lib/initial-profile";
+import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
+import React from "react";
 
 const SetupPage = async () => {
   const profile = await initialProfile();
@@ -20,7 +21,7 @@ const SetupPage = async () => {
     return redirect(`servers/${server.id}`);
   }
 
-  return <div>Create a server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
