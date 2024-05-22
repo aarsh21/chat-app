17845590cdac60ed69b5c4ee57ed49578702046b
aarsh21 <aarshpersonal21@gmail.com>
2024-05-22 08:51:09 +0530

    add: add theme provider
    Signed-off-by: aarsh21 <aarshpersonal21@gmail.com>

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { ClerkProvider, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
